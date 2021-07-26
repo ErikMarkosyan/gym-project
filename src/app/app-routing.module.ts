5badfd33f5449c1cae374d3cfaf6b0ef78b515b1
ErikMarkosyan <er001.em@gmail.com>
2021-07-26 17:48:10 +0400

    created the full project without lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,13 +11,13 @@ import { ContactUsComponent } from './contact-us/contact-us.component';
 
 const routes: Routes = [
   { path: '',   redirectTo: 'home', pathMatch: 'full' },
-  {path: 'home', component: HomeComponent },
-  {path: 'about', component: AboutComponent },
-  {path: 'service', component: ServicesComponent },
-  {path: 'news', component: NewsComponent },
-  {path: 'trainer', component: TrainerComponent },
-  {path: 'gallery', component: GalleryComponent },
-  {path: 'contact-us', component: ContactUsComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'service', component: ServicesComponent },
+  { path: 'news', component: NewsComponent },
+  { path: 'trainer', component: TrainerComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'contact-us', component: ContactUsComponent },
 ];
 
 @NgModule({
